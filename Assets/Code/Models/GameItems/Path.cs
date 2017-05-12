62f20cac7df16baba80b56bb660a8420a890cb65
Onur Alsaran <onuralsaran@gmail.com>
2017-05-12 14:08:56 +0300

    Add calculating points

--- Assets/Code/Models/GameItems/Path.cs
+++ Assets/Code/Models/GameItems/Path.cs
@@ -6,6 +6,9 @@ namespace Com.Yosi.Linkar.Code.Models.GameItems
 {
     public class Path
     {
+        private const int FINISHER_BONUS = 15;
+        private const int CARD_BONUS = 5;
+
         public List<Card> Cards { get; private set; }
 
         public Path(List<Point> points, Board board)
@@ -16,6 +19,27 @@ namespace Com.Yosi.Linkar.Code.Models.GameItems
         public override string ToString()
         {
             return String.Join("\n", Cards.Select(c => "Color: " + c.Color.ToString() + " -> " + c.Position.X + "-" + c.Position.Y).ToArray());
+        }
+
+        public Dictionary<CardType, int> GetPoints()
+        {
+            var points = new Dictionary<CardType, int>();
+            points.Add(CardType.Blue, 0);
+            points.Add(CardType.Red, 0);
+            points.Add(CardType.Green, 0);
+            points.Add(CardType.Yellow, 0);
+
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                if (Cards[i].Color != null)
+                {
+                    points[(CardType)Cards[i].Color] += CARD_BONUS;
+                }
+            }
+
+            points[(CardType)Cards[0].Color] += FINISHER_BONUS;
+
+            return points;
         }
     }
 }
--- Assets/Code/Test/TestScript.cs
+++ Assets/Code/Test/TestScript.cs
@@ -13,17 +13,24 @@ public class TestScript : MonoBehaviour
         _board.TargetPosition = new Point(5, 4);
 
         addCard(7, 8, CardType.Red);
-        addCard(8, 8, CardType.Red);
-        addCard(6, 6, CardType.Red);
-        addCard(6, 7, CardType.Red);
-        addCard(6, 5, CardType.Red);
-        addCard(6, 4, CardType.Red);
-        addCard(5, 4, CardType.Red);
+        addCard(8, 8, CardType.Blue);
+        addCard(6, 6, CardType.Blue);
+        addCard(6, 7, CardType.Yellow);
+        addCard(6, 5, CardType.Green);
+        addCard(6, 4, CardType.Green);
+        addCard(5, 4, CardType.Blue);
         addCard(5, 6, CardType.Red);
         addCard(5, 5, CardType.Red);
 
         var a = _board.GetClosestPathsToTarget();
         Debug.Log(a[0].ToString());
+
+        var points = a[0].GetPoints();
+        Debug.Log("Points:");
+        Debug.Log("Blue  : " + points[CardType.Blue]);
+        Debug.Log("Red   : " + points[CardType.Red]);
+        Debug.Log("Green : " + points[CardType.Green]);
+        Debug.Log("Yellow: " + points[CardType.Yellow]);
     }
 
     private void addCard(int x, int y, CardType type)
